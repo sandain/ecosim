80985771f46ff4a6f504b1f7454d0834ef74bb98
Jason M. Wood <sandain@hotmail.com>
2014-05-11 09:56:31 -0600

    Wrap the expected output in verbatim tags

--- src/fortran/tmatrix.f90
+++ src/fortran/tmatrix.f90
@@ -57,6 +57,8 @@
 !>
 !> Expected output:
 !>
+!> @verbatim
+!>
 !>      [01][02][03][04][05][06][07][08][09][10]
 !> [01]   1
 !> [02]   2   3
@@ -68,6 +70,8 @@
 !> [08]  29  30  31  32  33  34  35  36
 !> [09]  37  38  39  40  41  42  43  44  45
 !> [10]  46  47  48  49  50  51  52  53  54  55
+!>
+!> @endverbatim
 !>
 !> @author Jason M. Wood
 !> @copyright GNU General Public License
