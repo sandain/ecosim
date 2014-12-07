95b0bdd0050fb9489a9e11ca46158e08ea5f1c9c
Jason M. Wood <sandain@hotmail.com>
2014-12-07 13:44:02 -0500

    Binning: add epsilon to the distance check

--- src/java/Binning.java
+++ src/java/Binning.java
@@ -40,6 +40,7 @@ public class Binning implements Runnable {
      *  @param tree The NewickTree object.
      */
     public Binning (MasterVariables masterVariables, NewickTree tree) {
+        this.masterVariables = masterVariables;
         this.tree = tree;
         bins = new ArrayList<BinLevel> ();
         hasRun = false;
@@ -143,7 +144,7 @@ public class Binning implements Runnable {
         distance += b.maximumDistanceFromLeafNode () + b.getDistance ();
         // Recurse on the child nodes if the maximum distance exceeds the
         // crit value, otherwise colapse this branch into one bin.
-        if (distance > 1.000d - crit) {
+        if (distance > 1.000d - crit + masterVariables.EPSILON) {
             for (NewickTreeNode child: children) {
                 num += getNumberBins (crit, child);
             }
@@ -153,6 +154,8 @@ public class Binning implements Runnable {
         }
         return num;
     }
+
+    private MasterVariables masterVariables;
 
     private NewickTree tree;
 
