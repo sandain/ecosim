82256312367814934d01ff80499713b12970b116
Jason M. Wood <sandain@hotmail.com>
2015-06-22 21:37:35 -0600

    TestNewickTree: test the removeDescendant code

--- tests/java/TestNewickTree.java
+++ tests/java/TestNewickTree.java
@@ -18,7 +18,9 @@ public class TestNewickTree {
 
     @Before
     public void setup () throws InvalidNewickException {
-        File treeFile = new File ("build/tests/java/assets/TestNewickTree.nwk");
+        File treeFile = new File (
+            "build/tests/java/assets/TestNewickTree.nwk"
+        );
         tree = new NewickTree (treeFile);
     }
 
@@ -88,6 +90,37 @@ public class TestNewickTree {
             root.isRootNode ()
         );
     }
+
+    @Test
+    public void testRemoveDescendant () throws InvalidNewickException {
+        NewickTree a = new NewickTree (testNewickTree);
+        NewickTree b = new NewickTree (
+            "(((C:0.1,D:0.1):0.2,B:0.3):0.3,E:0.5):0.0;"
+        );
+        a.removeDescendant ("A" );
+        assertEquals (
+            "Remove descendant failed:\n" +
+            a.toString () + "\n" + b.toString () + "\n",
+            0,
+            a.compareTo (b)
+        );
+    }
+
+    @Test
+    public void testRemoveOutgroup () throws InvalidNewickException {
+        NewickTree a = new NewickTree (testNewickTree);
+        NewickTree b = new NewickTree (
+            "((A:0.1,B:0.2):0.1,(C:0.1,D:0.1):0.2):0.3;"
+        );
+        a.removeDescendant ("E");
+        assertEquals (
+            "Remove outgroup failed:\n" +
+            a.toString () + "\n" + b.toString () + "\n",
+            0,
+            a.compareTo (b)
+        );
+    }
+
 
     private NewickTree tree;
 
